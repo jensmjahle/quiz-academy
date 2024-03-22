a8312c5336c688f7922de1fd65151822b22c899a
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2024-03-22 19:02:22 +0100

    Create NavigationBar.vue and basic homepage

--- frontend/quizAcademyFrontend/src/App.vue
+++ frontend/quizAcademyFrontend/src/App.vue
@@ -1,18 +1,30 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import NavigationBar from "./components/NavigationBar.vue";
+import HomepageView from "./views/HomepageView.vue";
 
 </script>
 
 <template>
   <div id = "app">
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-    </nav>
-    <RouterView />
+    <div id = "header">
+      <h1>Quiz Academy</h1>
+      <h5>Welcome to Quiz Academy! Here you can take quizzes, create your own quizzes and much more!</h5>
+    </div>
+    <navigation-bar />
+
+    <div id = "body">
+      <router-view />
+    </div>
+    <div id = "footer">
+      <p>Quiz Academy 2021</p>
+    </div>
   </div>
 
 </template>
 
 <style scoped>
+
+
 
 </style>
---
+++ frontend/quizAcademyFrontend/src/views/HomepageView.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+import NavigationBar from "../components/NavigationBar.vue";
+</script>
+
+<template>
+  <div id = "body">
+    <h2>Home</h2>
+    <p>Welcome to Quiz Academy! Here you can take quizzes, create your own quizzes and much more!</p>
+    <h1>Quizzes</h1>
+    <h1>Quizzes</h1>
+    <h1>Quizzes</h1>
+    <h1>Quizzes</h1>
+    <h1>Quizzes</h1>
+    <h1>Quizzes</h1>
+    <h1>Quizzes</h1>
+    <h1>Quizzes</h1>
+    <h1>Quizzes</h1>
+    <h1>Quizzes</h1>
+    <h1>Quizzes</h1>
+    <h2>skerraaa</h2>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
