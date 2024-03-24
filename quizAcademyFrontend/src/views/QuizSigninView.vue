a7b4f57b13175719c2c46f0911e8d7682a6c7b0c
psued <psuedoari@gmail.com>
2024-03-24 13:32:15 +0100

    Made a sign up page that is almost identical to log in page, actual functionality will be added later

---
+++ quizAcademyFrontend/src/views/QuizSigninView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="quiz-view">
+    <QuizSignin />
+  </div>
+</template>
+
+<script>
+import QuizSignin from '../components/QuizSignIn.vue';
+
+export default {
+  components: {
+    QuizSignin
+  }
+};
+</script>
+
+<style>
+.quiz-view {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
