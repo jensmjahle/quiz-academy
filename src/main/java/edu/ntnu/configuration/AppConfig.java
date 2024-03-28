9df6baec8e1194e6aa41c5b1d20a5b3455751d4d
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2024-03-28 16:02:02 +0100

    Divide User handling into UserController and UserService

--- src/main/java/edu/ntnu/configuration/AppConfig.java
+++ src/main/java/edu/ntnu/configuration/AppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+
   @Bean
   public ModelMapper modelMapper() {
     return new ModelMapper();
--- src/main/java/edu/ntnu/controller/UserController.java
+++ src/main/java/edu/ntnu/controller/UserController.java
@@ -1,6 +1,7 @@
 package edu.ntnu.controller;
 
 import edu.ntnu.dto.UserDTO;
+import edu.ntnu.service.UserService;
 import java.util.logging.Logger;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,50 +16,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import edu.ntnu.repositories.UserRepository;
 import edu.ntnu.entities.User;
+import edu.ntnu.dto.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 @CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
-  Logger logger = Logger.getLogger(UserController.class.getName());
+  private final UserService userService;
 
-  private final ModelMapper modelMapper = new ModelMapper();
   @Autowired
-  private UserRepository userRepository;
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
 
   @GetMapping("/{username}")
   public ResponseEntity getUserByUsername(@PathVariable String username) {
-    User user = userRepository.findByUsername(username);
-    if (user != null) {
-      return new ResponseEntity<>(user, HttpStatus.OK);
-    } else {
-      return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
-    }
+    return userService.getUserByUsername(username);
   }
-
-  /**
-   * Create a new user
-   *
-   * @param userDTO UserDTO object
-   * @return ResponseEntity<String>
-   */
 
   @PostMapping()
   public ResponseEntity<String> createUser(@RequestBody UserDTO userDTO) {
-    logger.info("Creating user");
-    User user = modelMapper.map(userDTO, User.class);
-
-    if (userRepository.findByUsername(user.getUsername()) != null) {
-      return new ResponseEntity<>("Username already exists", HttpStatus.CONFLICT);
-    } else {
-      try {
-        userRepository.save(user);
-        return new ResponseEntity<>("User created successfully", HttpStatus.CREATED);
-      } catch (Exception e) {
-        return new ResponseEntity<>("Error creating user", HttpStatus.INTERNAL_SERVER_ERROR);
-      }
-    }
+    return userService.createUser(userDTO);
   }
-
 }
