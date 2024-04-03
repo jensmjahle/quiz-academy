a359a20f3143dead0a7d25ada6b5f6415a625b08
Ole Fredrik Høivang Heggum <ole0401@hotmail.com>
2024-04-03 18:23:51 +0200

    adds pinia state storage to the app

--- quizAcademyFrontend/src/main.js
+++ quizAcademyFrontend/src/main.js
@@ -1,9 +1,14 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import "./assets/main.css";
 import App from "./App.vue";
 import router from "./router";
 
+
+
 const app = createApp(App);
+const pinia = createPinia();
 
 app.use(router);
+app.use(pinia);
 app.mount("#app");
