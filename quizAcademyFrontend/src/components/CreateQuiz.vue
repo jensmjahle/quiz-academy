255d9e6693d10f8f1653ac2de453523942e7ed97
Ole Fredrik Høivang Heggum <ole0401@hotmail.com>
2024-04-07 22:04:27 +0200

    final fix

--- quizAcademyFrontend/src/components/CreateQuiz.vue
+++ quizAcademyFrontend/src/components/CreateQuiz.vue
@@ -20,6 +20,8 @@ const quizDescription = ref('');
 const quizTags = ref([]);
 const allTags = ref();
 const selectedTag = ref(null);
+let quizPhoto = ref(null);
+let imageUploaded = ref(false);
 
 const user = tokenStore.loggedInUser.data.username;
 
@@ -34,6 +36,10 @@ const initializeData = async () => {
             quizDescription.value = quizStore.quizDescription;
             quizPublicStatus.value = quizStore.quizPublicStatus;
             quizTags.value = quizStore.quizTags;
+            quizPhoto.value = quizStore.quizImage;
+            if(quizStore.quizImage !== null) {
+                imageUploaded.value = true;
+            }
             if (Array.isArray(quizStore.quizQuestions)) {
                 questions.value = quizStore.quizQuestions;
             } else {
@@ -49,10 +55,22 @@ const initializeData = async () => {
 };
 
 onMounted(() => {
-    initializeData(); // Call the async function when component is mounted
+    initializeData();
 });
 
-console.log()
+const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = () => {
+        quizPhoto.value = reader.result;
+        console.log("quizPhoto during onload: ", quizPhoto.value)
+        quizStore.setQuizPhoto(quizPhoto.value);
+    };
+    reader.readAsDataURL(file);
+    imageUploaded.value = true
+};
 
 const addTagToQuiz = () => {
     const selectedTagObj = selectedTag.value;
@@ -79,7 +97,7 @@ const createQuiz = async () => {
         quizId.value = response.data.quizId;
         quizDescription.value = response.data.quizDescription;
         quizCreated.value = true;
-        quizStore.initializeQuiz(quizId.value, quizName.value, questions.value, quizDescription.value, quizPublicStatus.value, quizTags.value);
+        quizStore.initializeQuiz(quizId.value, quizName.value, questions.value, quizDescription.value, quizPublicStatus.value, quizTags.value, quizPhoto.value);
         console.log(quizCreated.value);
     } catch (error) {
         console.error(error);
@@ -107,6 +125,7 @@ const updateQuiz = async () => {
         user: user,
         tags: quizTags.value,
         isPublic: quizPublicStatus.value,
+        quizImage: quizPhoto.value,
         quizCreationDate: date,
         questions: quizStore.quizQuestions,
     };
@@ -165,7 +184,14 @@ const resetWithConfirm = () => {
                 <input type="checkbox" id="is_public" name="is_public" v-model="quizPublicStatus" value="is_public">
             </div>
         </div>
-
+        <div id="add_picture" v-if="quizCreated">
+            <h5>Add or change picture for your question:</h5>
+            <div v-if="imageUploaded">
+                <h5 >Current image</h5>
+                <img :src="quizPhoto" alt="Question image"/>
+            </div>
+            <input type="file" @change="handleFileUpload" accept="image/*" />
+        </div>
         <div id="question_creation" v-if="quizCreated">
             <hr>
             <h5>Add questions to your quiz:</h5>
@@ -277,10 +303,10 @@ const resetWithConfirm = () => {
     margin-left: 15px;
     margin-right: 15px;
     -webkit-appearance: none;
-    width: 20px; /* Adjust the value as per your needs */
-    height: 20px; /* Adjust the value as per your needs */
-    background-color: var(--fifth-color); /* Adjust the color as per your needs */
-    border-radius: 50%; /* This will make the checkbox completely round */
+    width: 20px;
+    height: 20px;
+    background-color: var(--fifth-color);
+    border-radius: 50%;
     cursor: pointer;
 }
 
@@ -291,11 +317,11 @@ const resetWithConfirm = () => {
 #is_public:checked::after {
     content: "";
     display: block;
-    width: 10px; /* Adjust the value as per your needs */
-    height: 10px; /* Adjust the value as per your needs */
-    background-color: var(--fourth-color); /* Adjust the color as per your needs */
-    border-radius: 50%; /* This will make the circle completely round */
-    margin: 5px; /* Adjust the value as per your needs */
+    width: 10px;
+    height: 10px;
+    background-color: var(--fourth-color);
+    border-radius: 50%;
+    margin: 5px;
 }
 
 #question_in_list:hover {
